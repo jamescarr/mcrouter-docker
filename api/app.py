a2372c7c751500b1c1ab91b1b594599bce31faef
James Carr <james.r.carr@gmail.com>
2014-09-19 09:21:49 -0500

    split out core and api

---
+++ api/app.py
@@ -0,0 +1,83 @@
+from os import environ as env
+import json
+from flask import Flask, request, jsonify, Response
+from pymemcache.client import Client
+
+MCROUTER_ADDR = 'MCROUTER_PORT_5000_TCP_ADDR'
+MCROUTER_PORT = 'MCROUTER_PORT_5000_TCP_PORT'
+app = Flask(__name__)
+
+
+client = None
+def create_client():
+    global client
+    if not client:
+        if env.has_key(MCROUTER_ADDR) and env.has_key(MCROUTER_PORT):
+            client = Client((
+                env.get(MCROUTER_ADDR),
+                int(env.get(MCROUTER_PORT))
+            ))
+        else:
+            raise Error("{} and {} are required environment variables. Please ensure these are set")
+    
+    return client
+
+
+@app.route("/")
+def root():
+    return jsonify(version='0.0.0')
+
+@app.route("/config", methods=["PUT"])
+def put_config():
+    f = open('/etc/mcrouter/mcrouter.json', 'w')
+    f.write(request.get_data())
+    f.close()
+    return jsonify(message='configuration saved')
+
+@app.route("/config", methods=["GET"])
+def get_config():
+    return serve_stat('__mcrouter__.preprocessed_config')
+    
+@app.route("/options", methods=["GET"])
+def get_options():
+    res = create_client().get('__mcrouter__.options')
+    retr_options = {}
+
+    options = res.split("\n")
+    for option in options:
+        (key, value) = option.split(" ")
+        retr_options[key] = value
+
+    return jsonify(**retr_options)
+
+
+@app.route("/options/<option>", methods=["GET"])
+def get_option(option):
+    res = create_client().get('__mcrouter__.options({})'.format(option))
+
+    return jsonify(**{option: res})
+
+@app.route("/route/<key>")
+def get_route(key):
+    res = create_client().get('__mcrouter__.route(set,{})'.format(key))
+    
+    return jsonify(
+            key=key, 
+            routes=res.split('\n')
+    )
+
+
+@app.route("/config/sources", methods=["GET"])
+def get_config_sources():
+    return serve_stat('__mcrouter__.config_sources_info')
+
+def serve_stat(key):
+    res = create_client().get(key)
+    return Response(res, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run(
+        host='0.0.0.0',
+        port=int(env.get('FLASK_PORT', '5000')),
+        debug=True
+   )
