44d103522a42d285d2b6d920c94f26fad209896d
James Carr <james.r.carr@gmail.com>
2014-10-14 13:11:30 -0500

    run app:app

--- api/run.sh
+++ api/run.sh
@@ -6,5 +6,5 @@ readonly WORKERS=${NUMBER_OF_WORKERS-"1"}
 echo $PORT
 echo $WORKERS
 
-gunicorn --access-logfile - --log-level debug --debug -b 0.0.0.0:$PORT -w $WORKERS app
+gunicorn --access-logfile - --log-level debug --debug -b 0.0.0.0:$PORT -w $WORKERS app:app
 
